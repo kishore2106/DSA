3c549214a3d31a8b341e0bd1d310dc5eefc1fa0b
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-03 09:01:13 +0530

    Example Func for Recursion

---
+++ Recursion/func.cpp
@@ -0,0 +1,18 @@
+// Example Func for Recursion
+
+#include<iostream>
+using namespace std;
+
+void func(int n) {
+    if (n > 0){
+        cout << n << endl; 
+        func(n - 1);
+    }
+}
+
+int main() {
+    int x = 3;
+    func(x);
+
+    return 0;
+}
