a60e19fab71d5e21cedc80c1a4f02a06d48adbcb
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-03 19:28:58 +0530

    Power(Exponent) using Recursion

---
+++ Recursion/powerRecursion.cpp
@@ -0,0 +1,29 @@
+// Power(Exponent) using Recursion
+
+#include<iostream>
+using namespace std;
+
+int pow(int m, int n) {
+    if (n == 0){
+        return 1;
+    } else {
+        return m * pow(m, n-1);
+    }
+}
+
+// Number of Recursive call is reduced
+int Rpow(int m, int n) {
+    if (n == 0) {
+        return 1;
+    } else if(n % 2 == 0) {
+        return pow(m * m, n / 2);
+    } else {
+        return m * pow(m * m, (n - 1) / 2);
+    }
+}
+
+int main() {
+    cout<<pow(2, 9)<<endl;
+    cout<<Rpow(2, 9)<<endl;
+    return 0;
+}
