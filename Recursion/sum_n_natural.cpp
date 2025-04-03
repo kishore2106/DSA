f2f67f772fc4db212183bcbffb9a0047a24e8b7b
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-03 18:54:57 +0530

    modify sum of n natural numbers using Recursion

--- Recursion/sum_n_natural.cpp
+++ Recursion/sum_n_natural.cpp
@@ -3,16 +3,33 @@
 #include<iostream>
 using namespace std;
 
+// int sum(int n) {
+//     if (n > 0) {
+//         return sum(n-1) + n;
+//     }
+//     return 0;
+// }
+
 int sum(int n) {
-    if (n > 0) {
-        return sum(n-1) + n;
+    if (n == 0) {
+        return 0;
     }
-    return 0;
+    return sum(n - 1) + n;
+}
+
+int Isum(int n) {
+    int s = 0;
+    int i;
+    for(i=1; i<=n; i++) {
+        s = s + i;
+    }
+    return s;
 }
 
 int main() {
 
     int n = 5;
     cout<<sum(n)<<endl;
+    cout<<Isum(n)<<endl;
     return 0;
 }
