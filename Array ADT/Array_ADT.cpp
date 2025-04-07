71366992a4b75ef1927e4bc9b275cbce23620566
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-07 18:54:00 +0530

    Get Set Max Min Operations

--- Array ADT/Array_ADT.cpp
+++ Array ADT/Array_ADT.cpp
@@ -77,6 +77,45 @@ int LinearSearch(struct Array *arr, int key){
     return -1;
 }
 
+// Get
+int Get(struct Array arr, int index) {
+    if(index >= 0 && index < arr.length) {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+// Set
+void Set(struct Array *arr, int index, int x) {
+    if(index >= 0 && index < arr->length) {
+        arr->A[index] = x;
+    }
+}
+
+// Max
+int Max(struct Array arr) {
+    int max = arr.A[0];
+    int i;
+    for (i=1; i<arr.length; i++) {
+        if(arr.A[i] > max){
+            max = arr.A[i];
+        }     
+    }
+    return max;
+}
+
+// Min
+int Min(struct Array arr) {
+    int min = arr.A[0];
+    int i;
+    for (i=1; i<arr.length; i++) {
+        if(arr.A[i] < min){
+            min = arr.A[i];
+        }     
+    }
+    return min;
+}
+
 // Binary Search using Loop
 int BinarySearch(struct Array arr, int key){
     int l, mid, h;
