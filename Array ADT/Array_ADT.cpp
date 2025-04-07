434e45222e74d05eb9cc31bef58e849caf05e603
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-07 17:55:50 +0530

    Binary Search using Loop

--- Array ADT/Array_ADT.cpp
+++ Array ADT/Array_ADT.cpp
@@ -77,15 +77,34 @@ int LinearSearch(struct Array *arr, int key){
     return -1;
 }
 
+// Binary Search
+int BinarySearch(struct Array arr, int key){
+    int l, mid, h;
+    l = 0;
+    h = arr.length - 1;
+
+    while(l <= h) {
+        mid = (l + h)/2;
+        if(key == arr.A[mid]) {
+            return mid;
+        } else if (key < arr.A[mid]) {
+            h = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+
 int main(){
     struct Array arr={{2,3,4,5,6},20,5};
     // int i, n;
     // append(&arr, 7);
     // insert(&arr, 2, 9);
     // cout<<Delete(&arr, 1)<<endl;
-    display(arr);
-    cout<<LinearSearch(&arr, 4)<<endl;
-
+    // display(arr);
+    // cout<<LinearSearch(&arr, 4)<<endl;
+    cout<<BinarySearch(arr, 8)<<endl;
 
     display(arr);
 
