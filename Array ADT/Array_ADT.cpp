83c63687479cd68e70b82fdcc1bb0bb523b51031
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-07 21:46:43 +0530

    Merging 2 sorted Array

--- Array ADT/Array_ADT.cpp
+++ Array ADT/Array_ADT.cpp
@@ -1,6 +1,7 @@
 // Array ADT and its operations
 
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 
 // properties
@@ -260,8 +261,32 @@ void Rearrange(struct Array *arr){
     }
 }
 
+// Merging 2 sorted Array
+struct Array* Merge(struct Array *arr1, struct Array *arr2) {
+    int i,j,k;
+    i=j=k=0;
+    struct Array *arr3 = (struct Array*)malloc(sizeof(struct Array));
+    
+    while(i<arr1->length && j<arr2->length) {
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        } else {
+            arr3->A[k++]=arr2->A[j++];
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++]=arr1->A[i++];
+    }
+    for(;j<arr2->length;j++){
+        arr3->A[k++]=arr2->A[j++];
+    }
+    arr3->length=arr1->length+arr2->length;
+    arr3->size=arr1->size+arr2->size;
+    return arr3;
+}
+
 int main(){
-    struct Array arr={{-2,-3,4,5,-6,10,-23,-15},20,8};
+    // struct Array arr={{-2,-3,4,5,-6,10,-23,-15},20,8};
     // int i, n;
     // append(&arr, 7);
     // insert(&arr, 2, 9);
@@ -281,9 +306,13 @@ int main(){
     // RightRotate(&arr);
     // InsertSort(&arr, 4);
     // cout<<isSorted(arr)<<endl;
-    Rearrange(&arr);
+    // Rearrange(&arr);
 
-    display(arr);
+    struct Array arr1={{2,5,10,15,25},10,5};
+    struct Array arr2={{3,4,7,18,20},10,5};
+    struct Array *arr3 = Merge(&arr1, &arr2);
+
+    display(*arr3);
 
     return 0;
 }
