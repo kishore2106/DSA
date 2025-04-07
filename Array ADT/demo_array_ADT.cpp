9fca3a8cdd70502522894d8246cec5afe9a10413
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-07 15:55:23 +0530

    Demo Array ADT

---
+++ Array ADT/demo_array_ADT.cpp
@@ -0,0 +1,39 @@
+// Demo Array ADT
+
+#include<iostream>
+using namespace std;
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array arr){
+    cout<<"Elements are: ";
+    for(int i=0; i<arr.length; i++) {
+        cout<<arr.A[i]<<" ";
+    }
+}
+
+int main(){
+    struct Array arr;
+    int i, n;
+    cout<<"Enter the size of array: ";
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    cout<<"Enter number of numbers: ";
+    cin>>n;
+
+    cout<<"Enter the elements of array\n";
+    for(i=0; i<n; i++){
+        cin>>arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+    return 0;
+}
