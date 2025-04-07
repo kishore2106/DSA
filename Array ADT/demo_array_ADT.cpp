8e8a8a2ca85d6f7d8ac007104f665d0b21e6084e
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-07 15:57:19 +0530

    Demo Array ADT using stack

--- Array ADT/demo_array_ADT.cpp
+++ Array ADT/demo_array_ADT.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 struct Array{
-    int *A;
+    // int *A;
+    int A[20];
     int size;
     int length;
 };
@@ -17,21 +18,21 @@ void display(struct Array arr){
 }
 
 int main(){
-    struct Array arr;
+    struct Array arr={{2,3,4,5,6},20,5};
     int i, n;
-    cout<<"Enter the size of array: ";
-    cin>>arr.size;
-    arr.A = new int[arr.size];
-    arr.length = 0;
+    // cout<<"Enter the size of array: ";
+    // cin>>arr.size;
+    // arr.A = new int[arr.size];
+    // arr.length = 0;
 
-    cout<<"Enter number of numbers: ";
-    cin>>n;
+    // cout<<"Enter number of numbers: ";
+    // cin>>n;
 
-    cout<<"Enter the elements of array\n";
-    for(i=0; i<n; i++){
-        cin>>arr.A[i];
-    }
-    arr.length = n;
+    // cout<<"Enter the elements of array\n";
+    // for(i=0; i<n; i++){
+    //     cin>>arr.A[i];
+    // }
+    // arr.length = n;
 
     display(arr);
 
