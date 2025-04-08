1f08c94392d0a81028c9c5ba9cb69a841c4530c0
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-08 07:47:57 +0530

    Union of Sorted Array

--- Array ADT/Array_ADT.cpp
+++ Array ADT/Array_ADT.cpp
@@ -285,6 +285,34 @@ struct Array* Merge(struct Array *arr1, struct Array *arr2) {
     return arr3;
 }
 
+// Set Operations
+// Union of Sorted Array 
+struct Array* Union(struct Array *arr1, struct Array *arr2) {
+    int i,j,k;
+    i=j=k=0;
+    struct Array *arr3 = (struct Array*)malloc(sizeof(struct Array));
+    
+    while(i<arr1->length && j<arr2->length) {
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        } else if(arr2->A[j]<arr1->A[i]) {
+            arr3->A[k++]=arr2->A[j++];
+        } else {
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++]=arr1->A[i++];
+    }
+    for(;j<arr2->length;j++){
+        arr3->A[k++]=arr2->A[j++];
+    }
+    arr3->length=k;
+    arr3->size=arr1->size+arr2->size;
+    return arr3;
+}
+
 int main(){
     // struct Array arr={{-2,-3,4,5,-6,10,-23,-15},20,8};
     // int i, n;
@@ -309,8 +337,11 @@ int main(){
     // Rearrange(&arr);
 
     struct Array arr1={{2,5,10,15,25},10,5};
-    struct Array arr2={{3,4,7,18,20},10,5};
-    struct Array *arr3 = Merge(&arr1, &arr2);
+    struct Array arr2={{3,5,7,15,18},10,5};
+    // struct Array *arr3 = Merge(&arr1, &arr2);
+    struct Array *arr3 = Union(&arr1, &arr2);
+
+
 
     display(*arr3);
 
