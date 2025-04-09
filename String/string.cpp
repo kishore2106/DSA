dd8b984409fbbe2f830c1b22f2d38a97bdb2eb1b
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-09 11:40:21 +0530

    Counting Number of vowerls and consonants

--- String/string.cpp
+++ String/string.cpp
@@ -42,15 +42,31 @@ void toggleCase(char *s){
     }
 }
 
+// Counting Number of vowerls and consonants
+void countVowelCons(char *s){
+    int i;
+    int vcount=0, ccount=0;
+    for(i=0; s[i]!='\0';i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' ) {
+            vcount++;
+        } else if((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122)) {
+            ccount++;
+        }
+    }
+    cout<<"Number of vowels: "<<vcount<<"\n Number of consonants: "<<ccount;
+}
+
 int main() {
     char name[] = "welcome";
     char name1[] = "WELCOME";
     char name2[] = "WeLcOmE";
+    char vowelCons[] = "How are you"; 
     // cout<<length(name);
     // toLower(name1);
     // toUpper(name);
-    toggleCase(name2);
-    cout<<name2;
+    // toggleCase(name2);
+    // cout<<name2;
+    countVowelCons(vowelCons);
 
     return 0;
 }
