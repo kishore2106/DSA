2154ae1dc2b2d7070f39be09fe7185f957caefa4
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-09 11:09:33 +0530

    toLower toUpper toggleCase procedure

--- String/string.cpp
+++ String/string.cpp
@@ -10,9 +10,47 @@ int length(char s[]){
     return i;
 }
 
+// Upper case to Lower case
+void toLower(char *s){
+    int i;
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]>=65 && s[i]<=90){
+            s[i]+=32;
+        }
+    }
+}
+
+// Lower case to Upper case
+void toUpper(char *s){
+    int i;
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]>=97 && s[i]<=122){
+            s[i]-=32;
+        }
+    }
+}
+
+// Toggle case
+void toggleCase(char *s){
+    int i;
+    for(i=0; s[i]!='\0'; i++){
+        if(s[i]>=65 && s[i]<=90){
+            s[i]+=32;
+        } else if(s[i]>='a' && s[i]<='z'){
+            s[i]-=32;
+        }
+    }
+}
+
 int main() {
     char name[] = "welcome";
-    cout<<length(name);
+    char name1[] = "WELCOME";
+    char name2[] = "WeLcOmE";
+    // cout<<length(name);
+    // toLower(name1);
+    // toUpper(name);
+    toggleCase(name2);
+    cout<<name2;
 
     return 0;
 }
