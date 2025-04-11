f684d17fb1f669f63871171e961daa9c9973835f
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-11 09:49:25 +0530

    Linked List Display RevDisplay RecurDisplay Create

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,60 @@
+// Linked List
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+}*first;
+
+void create(int A[], int n){
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(i=1; i<n; i++){
+        t = new Node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+void Display(struct Node *p){
+    while(p != NULL){
+        cout<<p->data<<"->";
+        p=p->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void RDisplay(struct Node *p){
+    if(p != NULL){
+        cout<<p->data<<"->";
+        RDisplay(p->next);
+    } else {
+        cout<<"NULL";
+    }
+    
+}
+
+void RevDisplay(struct Node *p){
+    if( p != NULL ){
+        RevDisplay(p->next);
+        cout<<p->data<<" ";
+    } 
+    
+}
+
+int main(){
+    int A[] = {3,5,7,10,15};
+    create(A, 5);
+
+    RevDisplay(first);
+    return 0;
+}
