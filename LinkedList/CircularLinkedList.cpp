b86ae7adb63afdfd493a936531bcdf2a7228217c
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-12 18:44:32 +0530

    Display Circular Linked List

--- LinkedList/CircularLinkedList.cpp
+++ LinkedList/CircularLinkedList.cpp
@@ -6,25 +6,37 @@ struct Node{
     Node* next;
 }*Head;
 
+// Create Circular Linked List
 void Create(int a[], int n){
     int i;
-    struct Node *t;
-    t=new Node;
-    t->data=a[0];
-    t->next=Head;
-    Head=t;
+    struct Node *t,*last;
+    Head=new Node;
+    Head->data=a[0];
+    Head->next=Head;
+    last=Head;
 
     for(i=1; i<n; i++){
         t=new Node;
         t->data=a[i];
-        Head->next=t;
-        t->next=Head;
+        t->next=last->next;
+        last->next=t;
+        last=t;
     }
+}
+
+// Display Circular LL
+void Display(struct Node *h){
+    do{
+        cout<<h->data<<" ";
+        h=h->next;
+    } while(h!=Head);
+    cout<<endl;
 }
 
 int main(){
     int a[] = {1, 2, 3, 4, 5};
     Create(a, 5);
 
+    Display(Head);
     return 0;
 }
