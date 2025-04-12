1c2788b8d9799b066f1180804218fed48965f4b4
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-12 21:04:37 +0530

    Length of Doubly Linked List

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -8,6 +8,7 @@ struct Node{
     struct Node *next;
 }*first=NULL;
 
+// Create Doubly Linked List
 void create(int a[], int n){
     struct Node *p,*t;
     first=new Node;
@@ -21,14 +22,24 @@ void create(int a[], int n){
         t->data=a[i];
         t->prev=p;
         t->next=NULL;
+        p->next=t;
         p=t;
     }
+}
 
+// Length of Doubly Linked List
+int Length(struct Node *p){
+    int count=0;
+    while(p!=NULL){
+        count++;
+        p=p->next;
+    }
+    return count;
 }
 
 int main(){
-    int a[] = {10, 20, 30, 40, 50};
-    create(a, 5);
-
+    int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    create(a, 10);
+    cout<<Length(first);
     return 0;
 }
