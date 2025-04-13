d3a2949ad599540b1f6b08322f5359c194fec482
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-13 07:46:35 +0530

    Reverse a DDL

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -91,13 +91,28 @@ int Delete(struct Node *p, int index){
     return x;
 }
 
+// Reverse a DDL
+void Reverse(struct Node *p){
+    struct Node *t;
+    while(p!=NULL){
+        t=p->next;
+        p->next=p->prev;
+        p->prev=t;
+        p=p->prev;
+        if(p!=NULL && p->next==NULL){
+            first=p;
+        }
+    }
+}
+
 int main(){
     int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     create(a, 10);
     // cout<<Length(first)<<endl;
     Display(first);
     // Insert(first, 10, 45);
-    cout<<Delete(first,4)<<endl;
+    // cout<<Delete(first,4)<<endl;
+    Reverse(first);
     Display(first);
     return 0;
 }
