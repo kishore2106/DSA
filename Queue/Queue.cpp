2eec7f1d7bc44b03895f2233ead83a2b434b3ede
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-15 18:46:54 +0530

    Display elements of Queue

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -39,10 +39,28 @@ int dequeue(struct Queue *q){
     return x;
 }
 
+// Display elements of Queue
+void display(struct Queue *q){
+    int x=-1;
+    for(int i=q->front+1; i<=q->rear; i++){
+        x = q->Q[i];
+        printf("%d ",x);
+    }
+    printf("\n");
+}
+
 int main(){
 
     struct Queue q;
     create(&q, 5);
+    enqueue(&q, 1);
+    enqueue(&q, 2);
+    enqueue(&q, 3);
+    display(&q);
+
+    dequeue(&q);
+
+    display(&q);
 
     return 0;
 }
