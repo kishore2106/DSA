c0d18aa587d5a032e7496d965211b9edc5b7a0c9
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-18 07:24:43 +0530

    Search element in Binary Search Tree

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -46,6 +46,21 @@ void Inorder(struct Node *p){
     }
 }
 
+// Search element in Binary Search Tree
+Node* Search(int key){
+    struct Node *t=root;
+    while(t!=NULL){
+        if(key<t->data){
+            t=t->lchild;
+        } else if(key>t->data){
+            t=t->rchild;
+        } else{
+            return t;
+        }
+    }
+    return NULL;
+}
+
 int main(){
     Insert(50);
     Insert(30);
@@ -55,6 +70,21 @@ int main(){
     Insert(60);
     Insert(80);
     Inorder(root);
+    printf("\n");
+    struct Node *res=Search(30);
+    if(res!=NULL){
+        printf("Key Found");
+    } else {
+        printf("Key Not Found");
+    }
+    res=Search(38);
+    printf("\n");
+    if(res!=NULL){
+        printf("Key Found");
+    } else {
+        printf("Key Not Found");
+    }
+
 
     return 0;
 }
