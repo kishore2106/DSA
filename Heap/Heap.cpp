afa5a98d0a2f578d0cc31d0a47f340e91e80e32b
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-20 19:55:10 +0530

    printing heap

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -29,7 +29,33 @@ void Insert(vector<int>& vec, int key){
     vec[i] = key;
 }
 
+// printing heap
+template <class T>
+void Print(T& vec, int n){
+    cout << "Max Heap: [" << flush;
+    for (int i=0; i<n; i++){
+        cout << vec[i] << flush;
+        if (i < n-1){
+            cout << ", " << flush;
+        }
+    }
+    cout << "]" << endl;
+}
+
 int main(){
 
+    int a[] = {45, 35, 15, 30, 10, 12, 6, 5, 20, 50};
+    InsertA(a, 9);
+    Print(a, sizeof(a)/sizeof(a[0]));
+    cout << endl;
+ 
+    // STL based
+    vector<int> v = {45, 35, 15, 30, 10, 12, 6, 5, 20};
+    Print(v, v.size());
+    v.reserve(15);  // Reserve space for 15 elements
+ 
+    Insert(v, 50);
+    Print(v, v.size());
+ 
     return 0;
 }
