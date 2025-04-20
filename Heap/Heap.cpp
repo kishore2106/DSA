b465e6f8e7bb8265d4787a682e25f56611d86e8c
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-20 19:51:27 +0530

    Inserting an element in Heap using vector

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -1,6 +1,7 @@
 // Heap Impl using array and vector
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
 // Inserting an element in Heap
@@ -12,6 +13,20 @@ void InsertA(int A[], int n){
         i = i % 2 == 0 ? (i/2)-1 : i/2;
     }
     A[i] = temp;
+}
+
+// STL vector based
+void Insert(vector<int>& vec, int key){
+    // Insert key at the end
+    auto i = vec.size();
+    vec.emplace_back(key);
+ 
+    // Rearrange elements: Indices are not DRY :-(
+    while (i > 0 && key > vec[i % 2 == 0 ? (i/2)-1 : i/2]){
+        vec[i] = vec[i % 2 == 0 ? (i/2)-1 : i/2];
+        i = i % 2 == 0 ? (i/2)-1 : i/2;
+    }
+    vec[i] = key;
 }
 
 int main(){
