afa0458192b9079b737b92956aa57e2dd9b98cd9
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-21 07:50:14 +0530

    Insertion Sort impl

--- Sort/Sort.c
+++ Sort/Sort.c
@@ -9,7 +9,7 @@ void Swap(int *x, int *y) {
 
 // Bubble sort impl
 void BubbleSort(int A[], int n){
-    int flag;
+    int flag; // adaptive
     for(int i=0; i<n-1; i++){
         flag=0;
         for(int j=0; j<n-i-1; j++){
@@ -22,10 +22,25 @@ void BubbleSort(int A[], int n){
     }
 }
 
+// Insertion sort 
+void InsertionSort(int A[], int n){
+    int i,j,x;
+    for(i=1; i<n; i++){
+        j=i-1;
+        x=A[i];
+        while(j>-1 && A[j]>x){
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=x;
+    }
+}
+
 int main(){
     int A[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(A)/sizeof(A[0]);
-    BubbleSort(A, n);
+    // BubbleSort(A, n);
+    InsertionSort(A, n);
 
     for(int i=0; i<n; i++){
         printf("%d ", A[i]);
