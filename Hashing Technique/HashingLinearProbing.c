99d384ed56664dd06ccf8ffaad5e42a47fc0f3bf
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-22 19:55:22 +0530

    Hashing Linear Probing Insert Impl

---
+++ Hashing Technique/HashingLinearProbing.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define SIZE 10
+
+int hash( int key){
+    return key%SIZE;
+}
+
+int probe(int H[], int key){
+    int index=hash(key);
+    int i=0;
+    while((H[key+i])%SIZE!=0){
+        i++;
+    }
+    return (index+i)%SIZE;
+    
+}
+
+void Insert(int H[], int key){
+    int index=hash(key);
+    if(H[index]!=0){
+        index = probe(H, key);
+    }
+    H[index]=key;
+
+}
+
+int main(){
+    int H[10]={0};
+    Insert(H, 12);
+    Insert(H, 25);
+    Insert(H, 35);
+    Insert(H, 26);
+
+    return 0;
+}
