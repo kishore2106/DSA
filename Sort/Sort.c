8484d9162740883517cb8dfba8f05c2bcf432823
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-04-22 05:48:49 +0530

    Merge Impl

--- Sort/Sort.c
+++ Sort/Sort.c
@@ -74,6 +74,29 @@ void QuickSort(int A[], int l, int h){
     }
 }
 
+// Merge Sort
+// Merge Impl
+void Merge(int A[], int l, int mid, int h){
+    int i,j,k;
+    int B[100];
+    
+    while(i<=mid && j<=h){
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else 
+            B[k++]=A[j++];
+    }
+    for(;i<=mid;i++){
+        B[k++]=A[i];
+    }
+    for(;j<=h;j++){
+        B[k++]=A[j];
+    }
+    for(i=l; i<=h; i++){
+        A[i]=B[i];
+    }
+}
+
 int main(){
     // int A[] = {64, 34, 25, 12, 22, 11, 90};
     // int n = sizeof(A)/sizeof(A[0]);
